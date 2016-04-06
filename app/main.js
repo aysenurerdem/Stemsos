bf994d5bf24780c7ef39a9a2e0d3c4d066504025
aysenurerdem <aysenur.erdem45@gmail.com>
2016-04-06 14:16:20 -0500

    added other angular pages

--- app/main.js
+++ app/main.js
@@ -18,11 +18,26 @@ app.config(['$routeProvider',
             controller: 'contractedCtrl'
         }).
 
-        when('/contracted', {
-            templateUrl: 'view/contracted.html',
-            controller: 'contractedCtrl'
+        when('/level', {
+            templateUrl: 'view/level.html',
+            controller: 'levelCtrl'
         }).
-            
+
+        when('/portfolio', {
+            templateUrl: 'view/portfolio.html',
+            controller: 'portfolioCtrl'
+        }).
+
+        when('/students', {
+            templateUrl: 'view/students.html',
+            controller: 'studentsCtrl'
+        }).
+
+        when('/schoolRoster', {
+            templateUrl: 'view/schoolRoster.html',
+            controller: 'schoolRosterCtrl'
+        }).
+
         otherwise({
             redirectTo: '/'
         });
@@ -36,11 +51,32 @@ app.controller('contentCtrl', function ($scope) {
 
 
 app.controller('dashboardCtrl', function ($scope) {
-    $scope.firstName = "Aysenur dashboard "
+    $scope.firstName = "dashboard "
 
 });
 
 app.controller('contractedCtrl', function ($scope) {
-    $scope.firstName = "Aysenur "
+    $scope.firstName = "contracted school page "
 
 });
+
+app.controller('levelCtrl', function ($scope) {
+    $scope.firstName = "level one submisson page"
+
+});
+
+app.controller('portfolio', function ($scope) {
+    $scope.firstName = "E-portfolio submisson page"
+
+});
+
+app.controller('students', function ($scope) {
+    $scope.firstName = "My students page"
+
+});
+
+app.controller('schoolRoster', function ($scope) {
+    $scope.firstName = "school Roster page"
+
+});
+
