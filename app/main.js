4c3ee63923bdd8dc852ca0f92965968188740f76
aysenurerdem <aysenur.erdem45@gmail.com>
2016-05-04 13:30:39 -0500

    latest version

--- app/main.js
+++ app/main.js
@@ -1,47 +1,40 @@
 /**
  * Created by aysenurerdem on 4/2/16.
  */
-var app = angular.module('stemsos', ["ngRoute", "ui.grid"])
+var app = angular.module('stemsos', ["ui.router", "ui.grid","ui.grid.edit","ngResource"]);
 
 
-app.config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider.
-        when('/dashboard', {
-            templateUrl: 'view/dashboard.html',
+app.config(function ($stateProvider,$urlRouterProvider) {
+
+    $stateProvider
+        .state('dashboard', {
+            url: '/dashboard',
+            templateUrl: "views/dashboard.html",
             controller: 'dashboardCtrl'
-        }).
-
-
-        when('/contracted', {
-            templateUrl: 'view/contracted.html',
-            controller: 'contractedCtrl'
-        }).
-
-        when('/level', {
-            templateUrl: 'view/level.html',
+        })
+        .state('levelOneSubmission', {
+            url: '/submission/level1',
+            templateUrl: "views/level.html",
             controller: 'levelCtrl'
-        }).
-
-        when('/portfolio', {
-            templateUrl: 'view/portfolio.html',
+        })
+        .state('levelTwoSubmission', {
+            url: '/submission/level2',
+            templateUrl: "views/portfolio.html",
             controller: 'portfolioCtrl'
-        }).
-
-        when('/students', {
-            templateUrl: 'view/students.html',
+        })
+        .state('students', {
+            url: '/students',
+            templateUrl: "views/students.html",
             controller: 'studentsCtrl'
-        }).
-
-        when('/schoolRoster', {
-            templateUrl: 'view/schoolRoster.html',
+        })
+        .state('school', {
+            url: '/roster',
+            templateUrl: "views/schoolRoster.html",
             controller: 'schoolRosterCtrl'
-        }).
+        })
+});
 
-        otherwise({
-            redirectTo: '/'
-        });
-    }]);
+
 
 app.controller('contentCtrl', function ($scope) {
     $scope.firstName = "Aysenur "
@@ -70,34 +63,81 @@ app.controller('portfolioCtrl', function ($scope) {
 
 });
 
-app.controller('studentsCtrl', function ($scope) {
-    $scope.firstName = "My students page"
+app.controller('studentsCtrl', function ($scope,myStudentService) {
+    $scope.firstName = "My students page";
 
-});
+    $scope.gridOptions = {};
 
-app.controller('schoolRosterCtrl', function ($scope) {
-    $scope.firstName = "school Roster page";
-    $scope.myData = [
-        {
-            "firstName": "Cox",
-            "lastName": "Carney",
-            "company": "Enormo",
-            "employed": true
-        },
-        {
-            "firstName": "Lorraine",
-            "lastName": "Wise",
-            "company": "Comveyer",
-            "employed": false
-        },
-        {
-            "firstName": "Nancy",
-            "lastName": "Waters",
-            "company": "Fuelton",
-            "employed": false
-        }
+    $scope.gridOptions.data = myStudentService.myStudents;
+    $scope.gridOptions.enableCellEditOnFocus = true;
+
+    $scope.gridOptions.columnDefs = [
+        { name: 'name', enableCellEdit: true },
+        { name: 'gender', enableCellEditOnFocus:true, width: 200  },
+        { name: 'school',width: 300 },
+        { name: 'grade', enableCellEdit: true,displayName: 'Grade', width: 200},
+        { name: 'email',enableCellEdit: true, width: 200}
+        
     ];
 
 });
 
+app.controller('schoolRosterCtrl', function ($scope,studentData,myStudentService) {
+    $scope.firstName = "school Roster page";
 
+    studentData.query(function(students) {
+
+        $scope.gridOptions.data = students;
+
+    }); //query() returns all the entries
+
+
+    $scope.gridOptions = {};
+
+
+    $scope.gridOptions.enableCellEditOnFocus = true;
+
+    $scope.gridOptions.columnDefs = [
+        { name: 'name', enableCellEdit: true },
+        { name: 'gender', enableCellEditOnFocus:true, width: 200  },
+        { name: 'school',width: 300 },
+        { name: 'grade', enableCellEdit: true,displayName: 'Grade', width: 200},
+        { name: 'email',enableCellEdit: true, width: 200},
+        {
+            name:'Action',
+            cellTemplate: '<div><button ng-hide="row.entity.assigned" ng-click="grid.appScope.assignStudent(row.entity)">Assign To me</button></div>'
+        }
+    ];
+
+
+    $scope.assignStudent = function (student) {
+
+        student.assigned = true;
+        myStudentService.assignStudent(student);
+    };
+
+
+
+
+});
+
+
+//Factory and service for communicating between controllers
+
+//Services are like objects you don't have to return anything
+//you can build like an object
+app.service('myStudentService', function () {
+
+    this.myStudents = [];
+    this.assignStudent = function (student) {
+
+        //
+        this.myStudents.push(student);
+    };
+
+});
+
+//difference of the factory , you have to return an object
+app.factory('studentData', function($resource) {
+    return $resource('data.json');
+});
